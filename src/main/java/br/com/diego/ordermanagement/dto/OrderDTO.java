b4807f227d6b953294b9d3f8fddd874848a2e63d
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2024-07-01 10:04:56 -0300

    lombok annotation DTO

--- src/main/java/br/com/diego/ordermanagement/dto/OrderDTO.java
+++ src/main/java/br/com/diego/ordermanagement/dto/OrderDTO.java
@@ -1,12 +1,18 @@
 package br.com.diego.ordermanagement.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO {
 
     private UUID id;
